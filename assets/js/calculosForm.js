78ec0c9261612b0e18a3557e445217efe78ad975
Juan Pablo de Jesus Figueroa Jaramillo <juanpablodejesusfigueroa@gmail.com>
2023-07-19 20:02:18 -0600

    formulario

--- assets/js/calculosForm.js
+++ assets/js/calculosForm.js
@@ -7,5 +7,65 @@ const calcularPersonas = () => {
   document.getElementById("totalPersonas").value = totalPersonas;
 };
 const calcularFormulario = () => {
-  document.getElementById("contenedor_calcular").style.display = "inherit";
+  const swalWithBootstrapButtons = Swal.mixin({
+    customClass: {
+      confirmButton: "btn btn-success",
+      cancelButton: "btn btn-danger",
+    },
+    buttonsStyling: false,
+  });
+
+  swalWithBootstrapButtons
+    .fire({
+      title:
+        "Para calcular el precio, usted acepta los términos y condciones?",
+      text: "¡No podrás revertir esto!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Si, acepto",
+      cancelButtonText: "No, acepto!",
+      reverseButtons: true,
+    })
+    .then((result) => {
+      if (result.isConfirmed) {
+        var form = document.getElementById("FormReservar");
+        const fd = new FormData(form);
+        fd.append("accion", "alta");
+        fetch("../models/ReservarModel.php", {
+          method: "POST",
+          body: fd,
+        })
+          .then((result) => result.text())
+          .then((result) => {
+            if (result == 1) {
+              swalWithBootstrapButtons.fire(
+                "Reserva exitosa!",
+                "Su reserva ha sido realizada.",
+                "success"
+              );
+              setTimeout(function () {
+                location.reload();
+              }, 3000);
+            } else {
+              swalWithBootstrapButtons.fire(
+                "Error",
+                "Hemos tenido un error a la base de datos o la conexión.",
+                "error"
+              );
+              // setTimeout(function () {
+              //   location.reload();
+              // }, 3000);
+            }
+          });
+      } else if (
+        /* Read more about handling dismissals below */
+        result.dismiss === Swal.DismissReason.cancel
+      ) {
+        swalWithBootstrapButtons.fire(
+          "Cancelado",
+          "Por favor calcula el precio de tu reservación",
+          "error"
+        );
+      }
+    });
 };
