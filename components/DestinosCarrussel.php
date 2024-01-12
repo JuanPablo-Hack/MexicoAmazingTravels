2f01d1ba32302997367949ecb7ce522fa51442d5
Juan Pablo Figueroa <juanpablodejesusfigueroa@gmail.com>
2024-01-11 18:50:43 -0600

    x

--- components/DestinosCarrussel.php
+++ components/DestinosCarrussel.php
@@ -16,60 +16,7 @@
                 <h5 class="thumbnail-mary-time"></h5>
             </div>
         </div>
-        <div class="owl-item">
-            <!-- Thumbnail Classic-->
-            <article class="thumbnail thumbnail-mary">
-                <div class="thumbnail-mary-figure">
-                    <img src="https://mexicolu.gruposoca.com.mx/images/chiapas_destinos.jpg" alt="" width="420px" height="250px" />
-                </div>
-                <div class="thumbnail-mary-caption">
-                    <a class="icon fl-bigmug-line-zoom60" href="https://mexicolu.gruposoca.com.mx/images/chiapas_destinos.jpg" data-lightgallery="item"><img src="https://mexicolu.gruposoca.com.mx/images/chiapas_destinos.jpg" alt="" width="420px" height="250px" /></a>
-                </div>
-            </article>
-            <div class="thumbnail-mary-description">
-                <h5 class="thumbnail-mary-project">
-                    <a href="#">Chiapas</a>
-                </h5>
-                <span class="thumbnail-mary-decor"></span>
-                <h5 class="thumbnail-mary-time"></h5>
-            </div>
-        </div>
-        <div class="owl-item">
-            <!-- Thumbnail Classic-->
-            <article class="thumbnail thumbnail-mary">
-                <div class="thumbnail-mary-figure">
-                    <img src="https://mexicolu.gruposoca.com.mx/images/yucatan_destinos.jpg" alt="" width="420px" height="250px" />
-                </div>
-                <div class="thumbnail-mary-caption">
-                    <a class="icon fl-bigmug-line-zoom60" href="https://mexicolu.gruposoca.com.mx/images/yucatan_destinos.jpg" data-lightgallery="item"><img src="https://mexicolu.gruposoca.com.mx/images/yucatan_destinos.jpg" alt="" width="420px" height="250px" /></a>
-                </div>
-            </article>
-            <div class="thumbnail-mary-description">
-                <h5 class="thumbnail-mary-project">
-                    <a href="#">Yucatán</a>
-                </h5>
-                <span class="thumbnail-mary-decor"></span>
-                <h5 class="thumbnail-mary-time"></h5>
-            </div>
-        </div>
-        <div class="owl-item">
-            <!-- Thumbnail Classic-->
-            <article class="thumbnail thumbnail-mary">
-                <div class="thumbnail-mary-figure">
-                    <img src="https://mexicolu.gruposoca.com.mx/images/centro-campeche.jpg" alt="" width="420px" height="250px" />
-                </div>
-                <div class="thumbnail-mary-caption">
-                    <a class="icon fl-bigmug-line-zoom60" href="https://mexicolu.gruposoca.com.mx/images/centro-campeche.jpg" data-lightgallery="item"><img src="https://mexicolu.gruposoca.com.mx/images/centro-campeche.jpg" alt="" width="420px" height="250px" /></a>
-                </div>
-            </article>
-            <div class="thumbnail-mary-description">
-                <h5 class="thumbnail-mary-project">
-                    <a href="#">Campeche</a>
-                </h5>
-                <span class="thumbnail-mary-decor"></span>
-                <h5 class="thumbnail-mary-time"></h5>
-            </div>
-        </div>
+       
     </div>
 </section>
 <script>
