2fff0f6e167f7680b78b8f8c3c837196a9b10b49
Mona Lisa <sistemas@porter.com.mx>
2024-02-06 09:18:28 -0600

    Subiendo cambios en español

--- assets/js/calculosForm.js
+++ assets/js/calculosForm.js
@@ -27,7 +27,7 @@ const calcularFormulario = () => {
   });
 };
 
-const aceptarTerminosCalc = () => {
+aceptarTerminos = () => {
   const swalWithBootstrapButtons = Swal.mixin({
     customClass: {
       confirmButton: "btn btn-success",
@@ -49,7 +49,11 @@ const aceptarTerminosCalc = () => {
     })
     .then((result) => {
       if (result.isConfirmed) {
-        calcularFormulario();
+        Swal.fire(
+          "Felicidades!",
+          "Has aceptado nuestros términos y condiciones!",
+          "success"
+        );
       } else if (
         /* Read more about handling dismissals below */
         result.dismiss === Swal.DismissReason.cancel
@@ -61,12 +65,4 @@ const aceptarTerminosCalc = () => {
         );
       }
     });
-};
-
-aceptarTerminos = () => {
-  Swal.fire(
-    "Felicidades!",
-    "Has aceptado nuestros términos y condiciones!",
-    "success"
-  );
 };
