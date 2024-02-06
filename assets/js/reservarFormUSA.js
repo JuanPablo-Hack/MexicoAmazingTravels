1bf14bced1d3085c795bb671ba3afbf0dc0af649
Mona Lisa <sistemas@porter.com.mx>
2024-02-06 09:13:36 -0600

    Subiendo cambios de idiomas

--- assets/js/reservarFormUSA.js
+++ assets/js/reservarFormUSA.js
@@ -17,7 +17,7 @@ const fechasDisponibles = () => {
 const cuposDisponibles = () => {
   var id_viaje = $("#fechas_disponibles").val();
   $.ajax({
-    url: "../models/CuposDisponibles.php",
+    url: "../models/CuposDisponiblesUSA.php",
     method: "POST",
     data: {
       id: id_viaje,
---
+++ models/CuposDisponiblesUSA.php
@@ -0,0 +1,9 @@
+<?php
+include '../config/DataBase.php';
+$id = $_POST['id'];
+$sql = "SELECT cupos_disponibles FROM viajes WHERE id = '$id'";
+$result = $conexion->query($sql);
+$row = $result->fetch_assoc();
+?>
+<input class="form-input" id="contact-phone-2" type="text" name="datos[]" value="<?php echo 'Space available: ' .
+    $row['cupos_disponibles']; ?>" readonly id="cuposdisponibles" />
