d2a6fbb2ff080fe492838b82e0ee926ceaafa409
Mona Lisa <sistemas@porter.com.mx>
2024-02-19 15:26:01 -0600

    Subiendo cambios necesarios

--- models/CuposDisponibles.php
+++ models/CuposDisponibles.php
@@ -5,7 +5,7 @@ $sql = "SELECT cupos_disponibles FROM viajes WHERE id = '$id'";
 $result = $conexion->query($sql);
 $row = $result->fetch_assoc();
 if ($row['cupos_disponibles'] <= 0) {
-    $row['cupos_disponibles'] = 'No cupos disponibles';
+    $row['cupos_disponibles'] = 0;
 }
 ?>
 <input class="form-input" id="contact-phone-2" type="text" name="datos[]" value="<?php echo 'Cupos Disponibles: ' .
